8b57828c75001b8a55363f42f9b5687cf7ebead9
Ido Frankel <idofrankel@gmail.com>
2022-10-02 14:25:30 +0000

    .

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -1,194 +1,69 @@
-// // gitprofile.config.js
-
-// const config = {
-//   github: {
-//     username: 'idofrankel', // Your GitHub org/user name. (Required)
-//     sortBy: 'stars', // stars | updated
-//     limit: 1, // How many projects to display.
-//     exclude: {
-//       forks: false, // Forked projects will not be displayed if set to true.
-//       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
-//     },
-//   },
-//   social: {
-//     linkedin: 'idofrankel',
-//     twitter: '',
-//     facebook: '',
-//     instagram: '',
-//     dribbble: '',
-//     behance: '',
-//     medium: '',
-//     dev: '',
-//     website: 'https://idofrankel.github.io',
-//     phone: '',
-//     email: 'ido.frankel@gmail.com',
-//   },
-//   skills: [
-//     'Python', 'C++', 'JS', 'SQL', 'Angular Framework', 'Git',
-//   ],
-//   experiences:  [
-//     {
-//       company: 'GE Healthcare',
-//       position: 'Software intern',
-//       from: 'September 2021',
-//       to: 'August 2022',
-//     },
-//     {
-//       company: 'Ofek 324 unit -IAF',
-//       position: 'Fullstack Web Developer',
-//       from: '2017',
-//       to: ' July 2020',
-//     },
-//   ],
-//   education: [
-//     {
-//       institution: 'Technion - Israel Institute of Technology',
-//       degree: 'B.S in Computer Science',
-//       from: '2020',
-//       to: 'current',
-//     },
-//   ],
-//   // Display blog posts from your medium or dev account. (Optional)
-//   blog: {
-//     source: '', // medium | dev
-//     username: '', // to hide blog section, keep it empty
-//     limit: 0, // How many posts to display. Max is 10.
-//   },
-//   googleAnalytics: {
-//     // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-//     id: '', // Please remove this and use your own tag id or keep it empty
-//   },
-//   hotjar: {
-//     id: '', //  Please remove this and use your own id or keep it empty
-//     snippetVersion: 6,
-//   },
-//   themeConfig: {
-//     defaultTheme: 'light',
-
-//     // Hides the switch in the navbar
-//     // Useful if you want to support a single color mode
-//     disableSwitch: false,
-
-//     // Should use the prefers-color-scheme media-query,
-//     // using user system preferences, instead of the hardcoded defaultTheme
-//     respectPrefersColorScheme: true,
-
-//     // Hide the ring in Profile picture
-//     hideAvatarRing: false,
-
-//     // Available themes. To remove any theme, exclude from here.
-//     themes: [
-//       'light',
-//       'dark',
-//     ],
-
-//     // Custom theme
-//     customTheme: {
-//       primary: '#fc055b',
-//       secondary: '#219aaf',
-//       accent: '#e8d03a',
-//       neutral: '#2A2730',
-//       'base-100': '#E3E3ED',
-//       '--rounded-box': '3rem',
-//       '--rounded-btn': '3rem',
-//     },
-//   },
-// };
-
-// export default config;
-
-
 // gitprofile.config.js
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'idofrankel', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    limit: 1, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
+    linkedin: 'idofrankel',
+    twitter: '',
     facebook: '',
     instagram: '',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    website: 'https://arifszn.github.io',
+    medium: '',
+    dev: '',
+    website: 'https://idofrankel.github.io',
     phone: '',
-    email: 'arifulalamszn@gmail.com',
+    email: 'ido.frankel@gmail.com',
   },
   skills: [
-    'PHP',
-    'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
-    'Bootstrap',
+    'Python', 'C++', 'JS', 'SQL', 'Angular Framework', 'Git',
   ],
-  experiences: [
+  experiences:  [
     {
-      company: 'Monstarlab Bangladesh',
-      position: 'Backend Engineer II',
+      company: 'GE Healthcare',
+      position: 'Software intern',
       from: 'September 2021',
-      to: 'Present',
+      to: 'August 2022',
     },
     {
-      company: 'My Offer 360 Degree',
-      position: 'Web Application Developer',
-      from: 'July 2019',
-      to: 'August 2021',
+      company: 'Ofek 324 unit -IAF',
+      position: 'Fullstack Web Developer',
+      from: '2017',
+      to: ' July 2020',
     },
   ],
   education: [
     {
-      institution: 'American International University-Bangladesh',
-      degree: 'Bachelor of Science',
-      from: '2015',
-      to: '2019',
-    },
-    {
-      institution: 'Cantonment College, Jessore',
-      degree: 'Higher Secondary Certificate (HSC)',
-      from: '2012',
-      to: '2014',
-    },
-    {
-      institution: 'Chowgacha Shahadat Pilot High School',
-      degree: 'Secondary School Certificate (SSC)',
-      from: '2007',
-      to: '2012',
+      institution: 'Technion - Israel Institute of Technology',
+      degree: 'B.S in Computer Science',
+      from: '2020',
+      to: 'current',
     },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 3, // How many posts to display. Max is 10.
+    source: '', // medium | dev
+    username: '', // to hide blog section, keep it empty
+    limit: 0, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-    id: 'G-WLLB5E14M6', // Please remove this and use your own tag id or keep it empty
+    id: '', // Please remove this and use your own tag id or keep it empty
   },
   hotjar: {
-    id: '2617601', //  Please remove this and use your own id or keep it empty
+    id: '', //  Please remove this and use your own id or keep it empty
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'corporate',
+    defaultTheme: 'light',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -196,7 +71,7 @@ const config = {
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
-    respectPrefersColorScheme: false,
+    respectPrefersColorScheme: true,
 
     // Hide the ring in Profile picture
     hideAvatarRing: false,
@@ -205,34 +80,6 @@ const config = {
     themes: [
       'light',
       'dark',
-      'cupcake',
-      'bumblebee',
-      'emerald',
-      'corporate',
-      'synthwave',
-      'retro',
-      'cyberpunk',
-      'valentine',
-      'halloween',
-      'garden',
-      'forest',
-      'aqua',
-      'lofi',
-      'pastel',
-      'fantasy',
-      'wireframe',
-      'black',
-      'luxury',
-      'dracula',
-      'cmyk',
-      'autumn',
-      'business',
-      'acid',
-      'lemonade',
-      'night',
-      'coffee',
-      'winter',
-      'procyon',
     ],
 
     // Custom theme
