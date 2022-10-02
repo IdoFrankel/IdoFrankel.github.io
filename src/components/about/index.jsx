25c7280b4a127a8262daa629c881c88a5f3ca6a7
Ido Frankel <idofrankel@gmail.com>
2022-10-02 17:12:35 +0000

    br

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -39,9 +39,9 @@ const About = ({ loading }) => {
                         <span className="relative border-l border-base-300 border-opacity-30 my-2 mx-4">
                         4th-year student at the CS faculty at the Technion, enrolled in the Big Data Analysis Program.
                         The focus of the program is to expand understanding int the fields of ML, Deep Learning, Image Processing, and NLP (was on the Dean's list on all last four semesters). 
-                        <br /> Previously held a software developer position at GE Healthcare. During that internship, I was writing utility scripts in Python for easier integration
+                        Previously held a software developer position at GE Healthcare. During that internship, I was writing utility scripts in Python for easier integration
                          and build, parallelizing testing components, and designing a web application (Vue.js).
-                         <br /> Before the Technion, I was a FullStack web developer (mainly Angular Framework) in the IAF's OFEK-324 unit for three years. 
+                         Before the Technion, I was a FullStack web developer (mainly Angular Framework) in the IAF's OFEK-324 unit for three years. 
                          During that time I had the opportunity to develop monitoring and management systems,
                          one of which was awarded a citation by OFEK-324 commander.
                         </span>
