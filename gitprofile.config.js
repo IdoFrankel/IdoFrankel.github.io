bfb21660797cd0afa0fda15351e0cd7c5dae249b
Ido Frankel <IdoFrankel@users.noreply.github.com>
2022-10-02 13:39:04 +0300

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,9 +2,9 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'idofrankel', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    limit: 1, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
@@ -17,14 +17,16 @@ const config = {
     instagram: '',
     dribbble: '',
     behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    website: 'https://idofrankel.github.io/',
-    phone: '+972 54-5406696',
+    medium: '',
+    dev: '',
+    website: 'https://idofrankel.github.io',
+    phone: '',
     email: 'ido.frankel@gmail.com',
   },
-  skills: ['Python', 'C++', 'JS', 'SQL', 'Angular Framework', 'Git'],
-  experiences: [
+  skills: [
+    'Python', 'C++', 'JS', 'SQL', 'Angular Framework', 'Git',
+  ],
+  experiences:  [
     {
       company: 'GE Healthcare',
       position: 'Software intern',
@@ -49,7 +51,7 @@ const config = {
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
     source: '', // medium | dev
-    username: '',
+    username: '', // to hide blog section, keep it empty
     limit: 0, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
@@ -61,7 +63,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'light',
+    defaultTheme: 'corporate',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -77,35 +79,8 @@ const config = {
     // Available themes. To remove any theme, exclude from here.
     themes: [
       'light',
-        'dark',
-        // 'cupcake',
-        'bumblebee',
-        // 'emerald',
-        // 'corporate',
-        // 'synthwave',
-        // 'retro',
-        // 'cyberpunk',
-        // 'valentine',
-        // 'halloween',
-        // 'garden',
-        // 'forest',
-        // 'aqua',
-        // 'lofi',
-        // 'pastel',
-        // 'fantasy',
-        // 'wireframe',
-        // 'black',
-        // 'luxury',
-        // 'dracula',
-        // 'cmyk',
-        // 'autumn',
-        // 'business',
-        // 'acid',
-        // 'lemonade',
-        // 'night',
-        // 'coffee',
-        // 'winter',
-        // 'procyon',
+      'dark',
+      'corporate'
     ],
 
     // Custom theme
